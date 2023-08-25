96898ca305be10f65f89d3351053286e94d6daf7
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-08-25 08:40:09 +0300

    create app bar of home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/views/home-page.dart';
 
 void main() {
   runApp(const NewsApp());
@@ -10,12 +11,9 @@ class NewsApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      )
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+  home: HomePage(),
     );
   }
 }
---
+++ lib/views/home-page.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'News',
+              style: TextStyle(color: Colors.black),
+            ),
+            Text(
+              'Cloud',
+              style: TextStyle(color: Colors.orange),
+            ),
+          ],
+        ),
+      ),
+      body: Column(),
+    );
+  }
+}
