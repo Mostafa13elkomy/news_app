98e7e052fc9bfa402395cd95c5baf7a8d338648e
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-08-28 16:26:27 +0300

    handling news list view

--- lib/views/home-page.dart
+++ lib/views/home-page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/widgets/categories_list_view.dart';
 import 'package:news_app/widgets/news_list_view.dart';
-import 'package:news_app/widgets/news_tile.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -9,24 +8,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              'News',
-              style: TextStyle(color: Colors.black),
-            ),
-            Text(
-              'Cloud',
-              style: TextStyle(color: Colors.orange),
-            ),
-          ],
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'News',
+                style: TextStyle(color: Colors.black),
+              ),
+              Text(
+                'Cloud',
+                style: TextStyle(color: Colors.orange),
+              ),
+            ],
+          ),
         ),
-      ),
-      body: NewsListView(),
-    );
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: CustomScrollView(
+            physics:const BouncingScrollPhysics(),
+            slivers: [
+              SliverToBoxAdapter(
+                child: CategoriesListView(),
+              ),
+              const SliverToBoxAdapter(
+                child: SizedBox(
+                  height: 32,
+                ),
+              ),
+              const NewsListView()
+            ],
+          ),
+        ));
   }
 }
-
