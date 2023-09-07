951240b7bd6586f01988aa288198866646b9184f
Mostafa13elkomy <mostafa.elkomy110@gmail.com>
2023-09-07 19:32:39 +0300

    create article model

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -3,14 +3,13 @@ import 'package:dio/dio.dart';
 class NewsServices {
   final Dio dio;
   NewsServices(this.dio);
-
   getGeneralNews() async {
     var response = await dio.get(
         'https://newsapi.org/v2/top-headlines?apiKey=096de7831df747cb96b3341440320831&country=eg&category=general');
     Map<String, dynamic> jsonData = response.data;
     List<dynamic> articles = jsonData['articles'];
     for (var article in articles) {
-      print(article['name']);
+      print(article["author"]);
     }
   }
 }
